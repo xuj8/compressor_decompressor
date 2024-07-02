82303e2d1f81b987589f85362f0e61eedd378f35
Jack Xu <xujack2001@gmail.com>
2024-07-02 11:07:56 -0400

    Added common headers in types.h and defined types for utf8_t and dynamic_bitset from boost

---
+++ include/types.h
@@ -0,0 +1,10 @@
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+#include <boost/dynamic_bitset.hpp>
+
+using utf8_t = uint32_t;
+using dynamic_bitset = boost::dynamic_bitset <uint8_t>;
+
